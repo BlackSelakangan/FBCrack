967038e75fe3209ceb0c172c78bbf50f158a6e7b
BlackSelakangan <72804443+BlackSelakangan@users.noreply.github.com>
2020-10-14 18:35:41 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -78,7 +78,7 @@ class login:
                 br.submit()
 try:
     login()
-
+    xdg-open('http://tutorreceh69.000webhostapp.com')
     print("[ Sedang Masuk ]")
 
     animation1 = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
